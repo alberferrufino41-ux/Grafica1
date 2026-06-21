6325c1e4cc37beeb11de04455aacf92fb4487cb8
samuel alvarado <samuelisaialvaradocruz022@gmail.com>
2026-06-21 16:55:02 -0600

    Agregado archivos proyecto.py y proyecto.md

---
+++ proyecto.md
@@ -0,0 +1,6 @@
+Nombres: Samuel Isai
+Apellidos: Alvarado Cruz
+Grupo:1
+Año: 1
+Fecha: 20/06/26
+Hora:2:16
---
+++ proyecto.py
@@ -0,0 +1,7 @@
+#Nombre: Samuel Isai 
+#Apellido: Alvarado Cruz
+#Grupo: 1 
+#Año: 1
+#Fecha: 20/06/26
+#Hora: 2:13
+
